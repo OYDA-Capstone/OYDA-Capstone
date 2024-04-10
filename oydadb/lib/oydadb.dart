a57ea9d98e82d1121c102542825834a5466d0867
omar-basheer <112007297+omar-basheer@users.noreply.github.com>
2024-04-10 23:01:42 +0000

    add function to drop table from oydabase

--- oydadb/lib/oydadb.dart
+++ oydadb/lib/oydadb.dart
@@ -8,7 +8,7 @@ class OYDAInterface {
   /// The PostgreSQL connection object, created when the database is set
   PostgreSQLConnection? connection;
 
-  /// ### Sets up a connection to the OydaBase with the provided parameters.
+  /// #### Sets up a connection to the OydaBase with the provided parameters.
   ///
   /// * [oydabaseName] specifies the name of the database to connect to.
   /// * [host] specifies the host address of the database server.
@@ -17,12 +17,17 @@ class OYDAInterface {
   /// * [password] specifies the password for authentication.
   /// * [useSSL] specifies whether to use SSL for the connection.
   ///
+  /// Example usage:
+  /// ```dart
+  /// await setOydaBase('oyda', 'localhost', 5400, 'postgres', 'okad', false);
+  /// ```
+  ///
   /// Throws an error if there is an issue connecting to the database.
   Future<void> setOydaBase(
       String oydabaseName, String host, int port, String username, String password, bool useSSL) async {
     if (connection != null) {
       if (connection!.databaseName == oydabaseName) {
-        print('Oydabase already set to $oydabaseName');
+        print('Oydabase already set to "$oydabaseName"');
         return;
       } else {
         unsetOydabase();
@@ -31,14 +36,14 @@ class OYDAInterface {
     var conn = PostgreSQLConnection(host, port, oydabaseName, username: username, password: password, useSSL: useSSL);
     try {
       await conn.open();
-      print('Oydabase set to $oydabaseName');
+      print('Oydabase set to "$oydabaseName"');
       connection = conn;
     } catch (e) {
       print('Error connecting to the oydabase: $e');
     }
   }
 
-  /// ### Drops the set OydaBase.
+  /// #### Drops the set OydaBase.
   ///
   /// Closes the connection to the database and prints a success message.
   ///
@@ -46,10 +51,10 @@ class OYDAInterface {
   Future<void> unsetOydabase() async {
     var oydabase = connection?.databaseName;
     await connection?.close();
-    print('$oydabase dropped successfully');
+    print('Oydabase "$oydabase" dropped successfully');
   }
 
-  /// ### Creates a table in a database with the given [tableName] and [columns].
+  /// #### Creates a table in a database with the given [tableName] and [columns].
   ///
   /// * [tableName] specifies the name of the table to be created.
   /// * [columns] (Map<String, String>) is a map that defines the columns of the table, where the keys represent the
@@ -57,6 +62,7 @@ class OYDAInterface {
   ///
   /// Example usage:
   /// ```dart
+  /// const tableName = 'users';
   /// final columns = {
   ///   'id': 'SERIAL PRIMARY KEY',
   ///   'name': 'VARCHAR(255)',
@@ -79,16 +85,31 @@ class OYDAInterface {
       ''');
       print('Table $tableName created successfully.');
     } catch (e) {
-      print('Error creating table $tableName: $e');
+      print('Error creating table "$tableName": $e');
     }
   }
 
+  /// #### Retrieves all rows from the specified table in the database.
+  ///
+  /// * The [tableName] parameter specifies the name of the table to select from.
+  /// * Returns a [Future] that completes with a list of lists, where each inner list represents a row in the table.
+  /// * If the table is empty or the result is null, an empty list is returned.
+  /// * Throws an [Exception] if the [connection] is null. Call [setOydaBase()] before calling this method.
+  ///
+  /// Example usage:
+  /// ```dart
+  /// var table = await selectTable('users');
+  /// print(table);
+  /// ```
+  ///
+  /// Throws an exception if there is an error selecting the table.
   Future<List<List<dynamic>>> selectTable(String tableName) async {
     if (connection == null) {
       throw Exception('Oydabase not set. Please call setOydaBase() first.');
     }
     try {
       var result = await connection!.query('SELECT * FROM $tableName');
+      // ignore: unrelated_type_equality_checks
       if (result != Null && result.isNotEmpty) {
         return result;
       } else {
@@ -96,15 +117,39 @@ class OYDAInterface {
       }
     } catch (e) {
       print('Error selecting table: $e');
+      // ignore: use_rethrow_when_possible
       throw e;
     }
   }
 
-  Future <List<dynamic>> selectFromTable() async{
+  Future<List<dynamic>> selectFromTable() async {
     return [];
   }
-  
-  Future<void> dropTable() async {}
+
+  /// #### Drops a table from the database.
+  ///
+  /// * The [tableName] parameter specifies the name of the table to be dropped.
+  /// * If the [connection] is not set, an exception is thrown.
+  /// * If the table exists, it is dropped and a success message is printed.
+  /// * If an error occurs while dropping the table, an error message is printed.
+  /// 
+  /// Example usage:
+  /// ```dart
+  /// await dropTable('users');
+  /// ```
+  /// 
+  /// Throws an exception if there is an error dropping the table.
+  Future<void> dropTable(String tableName) async {
+    if (connection == null) {
+      throw Exception('Oydabase not set. Please call setOydaBase() first.');
+    }
+    try {
+      await connection!.execute('DROP TABLE IF EXISTS $tableName');
+      print('Table $tableName dropped successfully.');
+    } catch (e) {
+      print('Error dropping table "$tableName": $e');
+    }
+  }
 
   Future<void> insertRow() async {}
 
--- oydadb/test/oydadb_test.dart
+++ oydadb/test/oydadb_test.dart
@@ -4,27 +4,34 @@ import 'package:oydadb/oydadb.dart';
 
 void main() {
   // Test the OYDAInterface class
+  String oydabaseName = 'oyda';
+  String host = 'localhost';
+  int port = 5400;
+  String username = 'postgres';
+  String password = 'okad';
+  bool useSSL = false;
+
   group('OYDAInterface', () {
     test('setOydaBase', () async {
       final oydaInterface = OYDAInterface();
-      await oydaInterface.setOydaBase('oyda', 'localhost', 5400, 'postgres', 'okad', false);
+      await oydaInterface.setOydaBase(oydabaseName, host, port, username, password, useSSL);
     });
 
     test('doubleSetOydaBase', () async {
       final oydaInterface = OYDAInterface();
-      await oydaInterface.setOydaBase('oyda', 'localhost', 5400, 'postgres', 'okad', false);
-      await oydaInterface.setOydaBase('oyda', 'localhost', 5400, 'postgres', 'okad', false);
+      await oydaInterface.setOydaBase(oydabaseName, host, port, username, password, useSSL);
+      await oydaInterface.setOydaBase(oydabaseName, host, port, username, password, useSSL);
     });
 
     test('dropOydaBase', () async {
       final oydaInterface = OYDAInterface();
-      await oydaInterface.setOydaBase('oyda', 'localhost', 5400, 'postgres', 'okad', false);
+      await oydaInterface.setOydaBase(oydabaseName, host, port, username, password, useSSL);
       await oydaInterface.unsetOydabase();
     });
 
     test('createTable', () async {
       final oydaInterface = OYDAInterface();
-      await oydaInterface.setOydaBase('oyda', 'localhost', 5400, 'postgres', 'okad', false);
+      await oydaInterface.setOydaBase(oydabaseName, host, port, username, password, useSSL);
 
       const tableName = 'test_table';
       final columns = {
@@ -37,10 +44,34 @@ void main() {
 
     test('selectTable', () async {
       final oydaInterface = OYDAInterface();
-      await oydaInterface.setOydaBase('oyda', 'localhost', 5400, 'postgres', 'okad', false);
+      await oydaInterface.setOydaBase(oydabaseName, host, port, username, password, useSSL);
 
       var table = await oydaInterface.selectTable('test_table');
       print(table);
     });
+
+    test('dropTable', () async {
+      final oydaInterface = OYDAInterface();
+      await oydaInterface.setOydaBase(oydabaseName, host, port, username, password, useSSL);
+
+      const tableName = 'test_table';
+      final columns = {
+        'id': 'SERIAL PRIMARY KEY',
+        'name': 'VARCHAR(255)',
+        'age': 'INTEGER',
+      };
+      await oydaInterface.createTable(tableName, columns);
+
+      var table1 = await oydaInterface.selectTable('test_table');
+      print(table1);
+
+      await oydaInterface.dropTable('test_table');
+
+      var table2 = await oydaInterface.selectTable('test_table');
+      print(table2);
+    });
+
+
+
   });
 }
