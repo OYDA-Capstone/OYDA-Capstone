94dc32541227d6b04917340dd33d3753ce193344
omar-basheer <112007297+omar-basheer@users.noreply.github.com>
2024-04-27 02:23:18 +0000

    add config file for postgrest api to db connection

--- packages/oydagrest/lib/src/oydagrest.dart
+++ packages/oydagrest/lib/src/oydagrest.dart
@@ -1,1 +1,38 @@
+// ignore_for_file: unused_field
+
 library oydagrest;
+
+// ignore: unused_import
+import 'package:http/http.dart';
+
+class Oydagrest {
+  static String? _baseUrl;
+  static String? _devKey;
+
+  static void setBaseUrl(String baseUrl) {
+    _baseUrl = baseUrl;
+  }
+
+  static void unsetBaseUrl() {
+    _baseUrl = null;
+  }
+
+  static void setDevKey(String key) {
+    _devKey = key;
+  }
+
+  static void unsetDevKey() {
+    _devKey = null;
+  }
+
+  Future<void> setOydabase(String? key, String baseUrl) async {
+    // check if the dev key is set
+    if (key == null) {
+      throw Exception('Developer Key is required to access the Oydabase.');
+    } else {
+      setBaseUrl(baseUrl);
+    }
+  }
+
+  Future<void> unsetOydabase() async {}
+}
