c1288e690714c4e1c2ed793397ac8007379e5948
omar-basheer <112007297+omar-basheer@users.noreply.github.com>
2024-05-18 10:57:06 +0000

    Update singleton for interface class

--- lib/src/oyda_interface.dart
+++ lib/src/oyda_interface.dart
@@ -2,6 +2,21 @@ import 'package:oydadb/oydadb.dart';
 
 /// This class provides the public interface for interacting with an Oydabase.
 class OYDAInterface {
+  /// The singleton instance of the `OYDAInterface` class.
+  static final OYDAInterface _instance = OYDAInterface._internal();
+
+  /// The factory constructor for the `OYDAInterface` class.
+  factory OYDAInterface() {
+    return _instance;
+  }
+
+  /// Creates an instance of the `OYDAInterface` class with the necessary default instances for Oydabase operations.
+  OYDAInterface._internal()
+      : connectionManager = ConnectionManager(),
+        utilities = Utilities(ConnectionManager()),
+        dataManager = DataManager(Utilities(ConnectionManager()), ConnectionManager()),
+        tableManager = TableManager(ConnectionManager(), Utilities(ConnectionManager()));
+
   /// The [ConnectionManager] instance responsible for managing the connection to the database.
   final ConnectionManager connectionManager;
 
@@ -13,13 +28,6 @@ class OYDAInterface {
 
   /// The [TableManager] instance responsible for managing table operations.
   final TableManager tableManager;
-
-  /// Creates an instance of the `OYDAInterface` class with the necessary default instances for Oydabase operations.
-  OYDAInterface()
-      : connectionManager = ConnectionManager(),
-        utilities = Utilities(ConnectionManager()),
-        dataManager = DataManager(Utilities(ConnectionManager()), ConnectionManager()),
-        tableManager = TableManager(ConnectionManager(), Utilities(ConnectionManager()));
 
   /// Sets the Oydabase connection with the specified parameters.
   Future<void> setOydaBase(
@@ -42,17 +50,17 @@ class OYDAInterface {
     await tableManager.dropTable(tableName);
   }
 
-  /// Checks if a table exists in the oydabase.
+  /// Checks if a table with the given [tableName] exists in the oydabase.
   Future<bool> tableExists(String tableName) async {
     return await tableManager.tableExists(tableName);
   }
 
-  /// Selects all tables from the oydabase.
+  /// Selects table with the given [tableName] from the oydabase.
   Future<List<List<dynamic>>> selectTable(String tableName) async {
     return await tableManager.selectTable(tableName);
   }
 
-  /// Inserts rows into a table in the oydabase.
+  /// Inserts rows into a table with the given [tableName] in the oydabase.
   Future<void> insertRows(String tableName, Map<String, String> values) async {
     await dataManager.insertRows(tableName, values);
   }
@@ -67,12 +75,12 @@ class OYDAInterface {
     await dataManager.deleteRows();
   }
 
-  /// Selects rows from a table in the oydabase.
+  /// Selects rows that satisfy the given [conditions] from a table with the given [tableName] in the oydabase.
   Future<List<dynamic>> selectRows(String tableName, List<Condition> condition) async {
     return await dataManager.selectRows(tableName, condition);
   }
 
-  /// Selects columns from a table in the oydabase.
+  /// Selects columns that satisfy the given [conditions] from a table with the given [tableName] in the oydabase.
   Future<List<dynamic>> selectColumns(String tableName, List<String> columns, [List<Condition>? conditions]) async {
     return await dataManager.selectColumns(tableName, columns, conditions);
   }
